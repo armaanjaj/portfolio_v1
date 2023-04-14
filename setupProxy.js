62e250413411df670e8a83eaf6902e8e06a816a8
armaanjaj <armaan.jaj@gmail.com>
2023-04-13 21:43:23 -0600

    modify setupProxy.js file

--- setupProxy.js
+++ setupProxy.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function(app) {
   app.use(
     createProxyMiddleware(["/api"], {
-      target: "http://localhost:3000/",
+      target: "https://armaancodes.com/",
       changeOrigin: true
     })
   );
