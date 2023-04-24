34e598ed4a34a8cb8cc2e5b6896594a6aed9f9e8
armaanjaj <armaan.jaj@gmail.com>
2023-04-24 02:57:46 -0600

    fix(test bug):delete vercel.json and edit useAxios base url

--- pages/index.js
+++ pages/index.js
@@ -23,7 +23,7 @@ export default function Home() {
     
     const { response, error } = useAxios({
         method: 'get',
-        url: '/api/getSkills',
+        url: '/getSkills',
         headers: JSON.stringify({ accept: '*/*' }),
     });
     
