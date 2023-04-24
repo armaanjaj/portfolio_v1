d73bdd4e5ecc65eb555fcb5b054c64fcecb1378b
Armaan Jaj <113659942+armaanjaj@users.noreply.github.com>
2023-04-23 19:36:49 -0600

    edit: change theme color

--- components/Meta.js
+++ components/Meta.js
@@ -11,7 +11,7 @@ function Meta({
                 name="viewport"
                 content="width=device-width, initial-scale=1"
             />
-            <meta name="theme-color" content="#8f33ff" />
+            <meta name="theme-color" content="#08001A" />
             <meta name="keywords" content={keywords} />
             <meta name="description" content={description} />
             <meta charSet="utf-8" />
