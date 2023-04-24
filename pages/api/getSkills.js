09f966401f8b18cd89457755c56697c0b31b8940
armaanjaj <armaan.jaj@gmail.com>
2023-04-24 03:15:17 -0600

    fix(test bug):use path module and process.cwd() to get current working directory

--- pages/api/getSkills.js
+++ pages/api/getSkills.js
@@ -1,12 +1,14 @@
 import * as fs from "fs";
+import path from "path";
 
 export default async function getProjects(req, res) {
-    let data = await fs.promises.readdir("data/skillData/skills");
+    
+    let data = await fs.promises.readdir(path.join(process.cwd(), '/data/skillData/skills'));
     let allSkills = [];
     let skills;
     for (let index = 0; index < data.length; index++) {
         const item = data[index];
-        skills = await fs.promises.readFile("data/skillData/skills/" + item, "utf-8");
+        skills = await fs.promises.readFile(path.join(process.cwd(), '/data/skillData/skills') + item, "utf-8");
         allSkills.push(JSON.parse(skills));
     }
 
